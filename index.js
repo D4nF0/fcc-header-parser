595846d00865aa3f6c80c9854dc66cec41a0dcf2
Daniel Fořt <danielfort2005@gmail.com>
2024-09-08 13:31:15 +0000

    feat: add /api/whoami functionality

--- index.js
+++ index.js
@@ -20,9 +20,22 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => { 
+
+  const ipaddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
 });
+
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
